3e9a916776c3e0e4311991f98147f0783a51e26d
xml2208 <mohinabonu2208@gmail.com>
2022-12-16 17:31:53 +0500

    setting a splash screen

---
+++ app/src/main/java/com/example/androidbooknomy/BooknomyApp.kt
@@ -0,0 +1,11 @@
+package com.example.androidbooknomy
+
+import android.app.Application
+
+class BooknomyApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/example/androidbooknomy/di/AppModule.kt
@@ -0,0 +1,1 @@
+package com.example.androidbooknomy.di
